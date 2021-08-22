56866292ce0937188caa59c17dccaedabce7a595
Ajay Kumar Gupta <2018csajay6754@poornima.edu.in>
2021-08-22 14:51:12 +0530

    index_controller_changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const indexController = (req, res)=>{
-    
+    console.log("header:",req.headers['x-forwarded-proto'])
+    console.log("NODE_ENV",process.env.NODE_ENV)
+    console.log(req.url)
     if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
+    console.log("redirect to https:",['https://',req.get('Host'),req.url].join(''))
     return res.redirect([
       'https://',
       req.get('Host'),
