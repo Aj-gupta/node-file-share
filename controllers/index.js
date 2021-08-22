f493043ab4d8c9a87110ad32835d079b6de36e72
Ajay Kumar Gupta <2018csajay6754@poornima.edu.in>
2021-08-22 14:46:29 +0530

    Added:index_controller_https_redirect

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,13 @@
 const indexController = (req, res)=>{
+    
+    if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
+    return res.redirect([
+      'https://',
+      req.get('Host'),
+      req.url
+    ].join(''));
+  }
+    
     res.sendFile('index.html');
 }
 
