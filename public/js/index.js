610fa779bca58517f08061599cb3a211a76f3b91
Ajay Kumar Gupta <gajay3954@gmail.com>
2023-08-13 04:24:09 +0530

    fix: project url in frontend

--- public/js/index.js
+++ public/js/index.js
@@ -16,9 +16,9 @@ const emailForm = document.querySelector("#emailForm");
 const toast = document.querySelector(".toast");
 
 // const baseURL = "http://localhost:3000"
-const baseURL = "https://myfile-share.herokuapp.com";
-const uploadURL = `${baseURL}/api/files`;
-const emailURL = `${baseURL}/api/files/send`;
+const baseURL = window.location.href;
+const uploadURL = `${baseURL}api/files`;
+const emailURL = `${baseURL}api/files/send`;
 
 const maxAllowedSize = 100 * 1024 * 1024; //100mb
 
